8c2a91509219bf6a958296f6b9cf2d5891dfd5c4
“darke <3327307668@qq.com>
2018-03-01 18:02:45 +0800

    添加即时消息

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -4,6 +4,10 @@ let fortune = require('./lib/fortune');
 let formidable = require('formidable');
 let fs = require('fs');
 let jqupload = require('jquery-file-upload-middleware');
+let credentials = require('./credentials');
+
+//邮箱正则表达式
+let VALID_EMAIL_REGEX = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
 
 //设置handlebars模板引擎
 let handlebars = require('express3-handlebars').create({
@@ -33,6 +37,10 @@ app.disable('x-powered-by');
 
 //解析post请求URL编码体的中间件
 app.use(require('body-parser')());
+//设置获取cookie中间件
+app.use(require('cookie-parser')(credentials.cookieSecret));
+//会话存储中间件
+app.use(require('express-session')());
 
 //天气组件的中间件
 app.use((req,res,next)=>{
@@ -53,6 +61,7 @@ app.use((req,res,next)=>{
 //静态资源中间件
 app.use(express.static(__dirname+'/public'));
 
+//jq文件上传路由
 app.use('/upload',(req,res,next)=>{
 	let now = Date.now();
 	jqupload.fileHandler({
@@ -65,6 +74,13 @@ app.use('/upload',(req,res,next)=>{
 		}
 	})(req,res,next);
 });
+//如果有即显消息，把它上传到上下文并清除它
+app.use((req,res,next)=>{
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
+
 
 //首页
 app.get(['','/home','/home.html'],(req,res)=>{
@@ -99,8 +115,48 @@ app.get(['/data/nursery-rhyme','/data/nursery-rhyme.html'],(req,res)=>{
 
 //提交表单
 app.get(['/newsletter','/newsletter.html'],(req,res)=>{
-	//CSRF会在后面添加，目前只提供一个虚拟值
-	res.render('newsletter',{csrf:'CSRF token goes here'});
+	let name = req.body.name || '',email = req.body.email || '';
+	console.log(email);
+	//输入验证
+	if(!email.match(VALID_EMAIL_REGEX)){
+		if(req.xhr){
+			return res.json({
+				error:'Invalid name email address.'
+			});
+		}
+		req.session.flash = {
+			type:'danger',
+			intro:'Validation error!',
+			message:'The email address you entered was not valid.'
+		};
+		return res.redirect(303,'/newsletter/archive');
+	}
+	new NewletterSignup({name:name,email:email}).save((err)=>{
+		if(err){
+			if(req.xhr){
+				res.json({
+					error:'Database error.'
+				});
+			}
+			req.session.flash = {
+				type:'danger',
+				intro:'Database',
+				message:'There was a database error;please try again later.'
+			}
+			return res.redirect(303,'/newsletter/archive');
+		}
+		if(req.xhr){
+			return res.json({success : true});
+		}
+		req.session.flash = {
+			type:'success',
+			intro:'thank you!',
+			message:'You have now been signed up for the newsletter.'
+		};
+		return res.redirect(303,'/newsletter/archive');
+	});	
+	// //CSRF会在后面添加，目前只提供一个虚拟值
+	// res.render('newsletter',{csrf:'CSRF token goes here'});
 });
 app.post(['/process','/process.html'],(req,res)=>{
 	if(req.xhr || req.accepts('json,html') === 'json'){
@@ -138,7 +194,6 @@ app.post('/contest/vacation-photo/:year/:month',(req,res)=>{
 app.get(['/uploadfile','/uploadfile.html'],(req,res)=>{
 	res.render('uploadfile');
 });
-
 
 
 //定制404页面
